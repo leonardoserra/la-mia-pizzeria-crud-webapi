eb34f5cea6365aa5dea89e2616ed76fa80a8e12d
leonardoserra <serra.leonardo92@gmail.com>
2023-10-09 15:12:55 +0200

    creato PizzaAPIController

---
+++ la-mia-pizzeria-crud-mvc/Controllers/API/PizzaAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_crud.Controllers.API
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzaAPIController : ControllerBase
+    {
+
+    }
+}
